ec525caa52f70f8064fb59865649c56e765b6652
AndrewBracero <a.bracero777@gmail.com>
2022-10-06 00:46:55 -0400

    Build Recipes stub api

---
+++ RecipesComponent.py
@@ -0,0 +1,11 @@
+from Shake import *
+from User import *
+
+#TODO find out exactly what parameter should be used
+def get_selected_recipes(ingredients: Ingredients) -> List[recipes]:
+    """
+    This method returns a list of of recipes that involve some or all of the elements in the parameter,
+    a list of ingredients. 
+    param: List of ingredients 
+    return: List of recipes
+    """
