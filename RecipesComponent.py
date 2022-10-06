f4ca313056683af54485e63d01d7b054d5ebd544
AndrewBracero <a.bracero777@gmail.com>
2022-10-06 00:57:20 -0400

    changed recipes component to reflect proper syntax

--- RecipesComponent.py
+++ RecipesComponent.py
@@ -3,7 +3,7 @@ from ingredients import Ingredients
 from recipes import Recipes
 
 #TODO find out exactly what parameter should be used
-def get_selected_recipes(ingredients: Ingredients) -> List[recipes]:
+def get_selected_recipes(ingredients: Ingredients) -> List[Recipes]:
     """
     This method returns a list of of recipes that involve some or all of the elements in the parameter,
     a list of ingredients. 
