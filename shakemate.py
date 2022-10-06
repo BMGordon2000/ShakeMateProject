35b76bf874f6a625dcedff234accae9fde33263e
Ben Gordon <bmgordon2000@gmail.com>
2022-10-05 20:59:06 -0400

    added ingredient and recipe templates and updated py file

--- shakemate.py
+++ shakemate.py
@@ -11,6 +11,10 @@ def index():
 @webapp.route('/ingredients')
 def ingredients():
     return render_template('Ingredients.html')
+
+@webapp.route('/recipes')
+def recipes():
+    return render_template('Recipes.html')
     
 if __name__ == "__main__":
     webapp.run(debug=True)
