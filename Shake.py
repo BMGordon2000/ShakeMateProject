4ab29aac8c5a8d69bf762716f9cbb140b06050ce
Ben Gordon <bmgordon2000@gmail.com>
2022-10-05 23:14:43 -0400

    added shake and ingredient classes

---
+++ Shake.py
@@ -0,0 +1,8 @@
+class Shake:
+    def __init__(self, ShakeID, Name, CaloricData, Fat, Recipe, DateAccessed):
+        self.shakeID = ShakeID
+        self.name = Name
+        self.caloricData = CaloricData
+        self.fat = Fat
+        self.recipe = Recipe
+        self.dateAccessed = DateAccessed
