a7856fe01f6ebc9e71460d8919bf72ba3f958ec8
Trafton <reynoldstrafton@gmail.com>
2022-10-06 00:53:14 -0400

    added databaseComponent with first method

---
+++ databaseComponent.py
@@ -0,0 +1,11 @@
+# Database Component
+
+from Ingredient import Ingredient
+
+def IngredientInfo(filter: List[Ingredient]) -> List[[Ingredient]]:
+    """
+    This method returns each ingredient and its nutrition info
+    :param filter: filters to load only selected ingredients, has no filter if empty list
+    :return: a multivalued list of ingredients and its nutrition information
+    """
+    pass
