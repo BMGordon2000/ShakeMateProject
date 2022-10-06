b8fd07260aeada05c7bb8109c3ffeb419e5c2f67
bryson705 <bkh6305@uncw.edu>
2022-10-05 21:52:13 -0400

    Added Account, Authentication, and Favorites/History in python

--- shakemate.py
+++ shakemate.py
@@ -15,6 +15,18 @@ def ingredients():
 @webapp.route('/recipes')
 def recipes():
     return render_template('Recipes.html')
+
+@webapp.route('/account')
+def account():
+    return render_template('Account.html')
+
+@webapp.route('/authenticate')
+def authenticate():
+    return render_template('Authentication.html')
+
+@webapp.route('/Fav_Hist')
+def fav_hist():
+    return render_template('Favorites_History.html')
     
 if __name__ == "__main__":
     webapp.run(debug=True)
