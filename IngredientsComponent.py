ebcd6cdc45c564efd64395aa04839a9e55c4f3cc
Ben Gordon <bmgordon2000@gmail.com>
2022-10-06 00:13:52 -0400

    added ingredient api

---
+++ IngredientsComponent.py
@@ -0,0 +1,11 @@
+from Shake import *
+from User import *
+
+
+def get_selected_ingredients():
+    """
+    This method returns a list of the selected ingredients on the ingredients page
+    :param: None
+    :return: A List of ingredient objects
+    """
+    pass
