8b6ce6b862f548bca19a5a1947e0e86f6e0c7ae4
Ben Gordon <bmgordon2000@gmail.com>
2022-10-05 23:01:43 -0400

    user class added

---
+++ User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, email, password):
+        self.userEmail = email
+        self.userPassword = password
+        self.favorites = []
+        self.history = []
+        
