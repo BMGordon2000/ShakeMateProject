262801a3787a8e64b722031a1b737d6134e9ddfc
AndrewBracero <a.bracero777@gmail.com>
2022-10-18 12:38:57 -0400

    Blueprints structure completed

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
 
-authentication = Blueprint("authentication", __name__, static_folder="static", template_folder="templates")
+authentication = Blueprint("authentication", __name__,
+                           static_folder="static", template_folder="templates")
 
-@authentication.route("/recipes")
+
+@authentication.route("/authenticate")
 @authentication.route("/")
 def index():
     return render_template("Authentication.html")
