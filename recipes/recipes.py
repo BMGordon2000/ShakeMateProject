88141891264319cfe6356c3de9b837a71c0a2435
AndrewBracero <a.bracero777@gmail.com>
2022-10-19 00:11:28 -0400

    TODO fix for loop for recipes

--- recipes/recipes.py
+++ recipes/recipes.py
@@ -5,17 +5,17 @@ recipes = Blueprint("recipes", __name__, static_folder="static", template_folder
 
 recipes_list = [
     {"id": 1, "name": "Brown shake with nuts", "calories": 250, "fat": 8, "sugar": 9 },
-    {"id": 1, "name": "Brown shake", "calories": 235, "fat": 7, "sugar": 7 },
-    {"id": 1, "name": "Dark green shake", "calories": 60, "fat": 1, "sugar": 2 },
-    {"id": 1, "name": "Dark orange shake", "calories": 120, "fat": 4, "sugar": 4 },
-    {"id": 1, "name": "Dark purple shake", "calories": 130, "fat": 7, "sugar": 6 },
-    {"id": 1, "name": "Dark red shake", "calories": 145, "fat": 2, "sugar": 7 },
-    {"id": 1, "name": "Light green shake", "calories": 90, "fat": 1, "sugar": 1 },
-    {"id": 1, "name": "Light orange shake", "calories": 160, "fat": 7, "sugar": 9 },
-    {"id": 1, "name": "Light purple shake", "calories": 140, "fat": 5, "sugar": 4 },
-    {"id": 1, "name": "Light red shake", "calories": 145, "fat": 4, "sugar": 3 },
-    {"id": 1, "name": "Light yellow shake", "calories": 180, "fat": 8, "sugar": 5 },
-    {"id": 1, "name": "White shake", "calories": 220, "fat": 10, "sugar": 12 },
+    {"id": 2, "name": "Brown shake", "calories": 235, "fat": 7, "sugar": 7 },
+    {"id": 3, "name": "Dark green shake", "calories": 60, "fat": 1, "sugar": 2 },
+    {"id": 4, "name": "Dark orange shake", "calories": 120, "fat": 4, "sugar": 4 },
+    {"id": 5, "name": "Dark purple shake", "calories": 130, "fat": 7, "sugar": 6 },
+    {"id": 6, "name": "Dark red shake", "calories": 145, "fat": 2, "sugar": 7 },
+    {"id": 7, "name": "Light green shake", "calories": 90, "fat": 1, "sugar": 1 },
+    {"id": 8, "name": "Light orange shake", "calories": 160, "fat": 7, "sugar": 9 },
+    {"id": 9, "name": "Light purple shake", "calories": 140, "fat": 5, "sugar": 4 },
+    {"id": 10, "name": "Light red shake", "calories": 145, "fat": 4, "sugar": 3 },
+    {"id": 11, "name": "Light yellow shake", "calories": 180, "fat": 8, "sugar": 5 },
+    {"id": 12, "name": "White shake", "calories": 220, "fat": 10, "sugar": 12 },
     ]
 @recipes.route("/recipes")
 @recipes.route("/")
