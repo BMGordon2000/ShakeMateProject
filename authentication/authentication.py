905ef957f19dca936cf78e2bfcd4237532660dc9
drakekvaall <94070273+drakekvaall@users.noreply.github.com>
2022-10-19 01:52:33 -0400

    added login_required
    login_required

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -17,7 +17,7 @@ def login():
         user = users.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Logged in was successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('account.Account'))
             else:
