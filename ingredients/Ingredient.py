c61f60e1c221d2dc110c0d4350613a3802cfa7d2
Ben Gordon <bmgordon2000@gmail.com>
2022-11-02 00:30:33 -0400

    cleaned up extra ingredient files

--- ingredients/Ingredient.py
+++
@@ -1,7 +0,0 @@
-class Ingredient:
-    def __init__(self, IngredientID, Name, CaloricData, Fat, Sugar):
-        self.ingredientID = IngredientID
-        self.name = Name
-        self.caloricData = CaloricData
-        self.fat = Fat
-        self.sugar = Sugar
