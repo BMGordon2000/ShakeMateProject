a795cee33ba020761cd2504ae9477846af8348b6
drakekvaall <94070273+drakekvaall@users.noreply.github.com>
2022-11-13 16:27:44 -0500

    fixed db with assocation table
    added the assocation table

--- app/DatabaseComponent.py
+++ app/DatabaseComponent.py
@@ -3,6 +3,7 @@
 # from User import *
 # from ingredients.Ingredient import *
 from flask_login import UserMixin
+import sqlalchemy as sa
 
 from app import db
 
@@ -18,10 +19,13 @@ class User(db.Model, UserMixin): ## creates the database table which stores the 
 
 
 
-# filter_table = db.Table(
-#     db.Column('recipe_table_id', db.Integer(), db.ForeignKey('recipe_table.id')),
-#     db.Column('ingredients_table_id', db.Integer(), db.ForeignKey('ingredients_table.id'))
-#      )
+filter_table = db.Table(
+    "filter_table",
+    # db.Column('recipe_table_id', db.Integer(), db.ForeignKey('recipe_table.id')),
+    # db.Column('ingredients_table_id', db.Integer(), db.ForeignKey('ingredients_table.id')),
+    db.Column('recipe_table_ingName', db.Integer(), db.ForeignKey('recipe_table.ingName')),
+    db.Column('ingredients_table_name', db.Integer(), db.ForeignKey('ingredients_table.name'))
+     )
 
 class ingredients_table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
