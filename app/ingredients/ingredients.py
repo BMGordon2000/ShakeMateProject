0b0080740dbdec66ad644c1c6a2a69f6ad4b176b
bryson705 <bkh6305@uncw.edu>
2022-11-14 20:37:02 -0500

    Update ingredients.py
    Clean up and minor fixes for adding for #30 and filtering for #31

--- app/ingredients/ingredients.py
+++ app/ingredients/ingredients.py
@@ -16,15 +16,11 @@ def getShakes():
     data = request.json["items"]
     for i in data:
         shake = recipe_table.query.filter_by(ingName=i).all()
+        # shakes.append(shake.name) # only use for .first()
         for j in shake:
             myShake = recipe_table.query.filter_by(ingName=i).first()
-            # print(myShake)
-            shakes.append(myShake.name)
-            # print(j)
-            # shakes.append(shake.name) # In the future shake.name would be added to the javascript that would point to the url that would contain the recipe link
-        # print(shake)
-        # shakes.append(shake.name) # In the future shake.name would be added to the javascript that would point to the url that would contain the recipe link 
-    # print(data)
+            shakes.append(myShake.name) # In the future shake.name would be added to the javascript that would point to the url that would contain the recipe link
+        # print(data)
     print(shakes)
     r = jsonify({"value":f"{shakes}"})
     r.status_code = 200
