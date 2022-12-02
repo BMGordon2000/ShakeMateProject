a67652278d91fbc7ce578a6b8f84f544cb3d58d2
drakekvaall <94070273+drakekvaall@users.noreply.github.com>
2022-12-01 21:10:26 -0500

    adding testing files
    added auth tests with login

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,8 +1,33 @@
-import pytest
 
+import pytest 
 from app.DatabaseComponent import User
 
-def test_auth():
+
+
+
+from app.__init__ import create_app
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+
+
+def test_new_user():
     user = User('drake@gmail.com','drake12345')
     assert user.email == 'drake@gmail.com'
     assert user.password == 'drake12345'
+    assert user.is_authenticated
+    assert user.is_active
+    assert not user.is_anonymous
+
+
+def test_new_user_with_fixture(new_user):
+    """
+    GIVEN a User model
+    WHEN a new User is created
+    THEN check the email and password_hashed fields are defined correctly
+    """
+    assert new_user.email == 'drake@gmail.com'
+    assert new_user.password == 'drake12345'
