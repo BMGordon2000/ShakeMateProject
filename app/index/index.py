35450fa68917983bbf5f10149df38dda801698a1
bryson705 <bkh6305@uncw.edu>
2022-12-02 22:50:39 -0500

    CSS and HTMl adjustments

--- app/index/index.py
+++ app/index/index.py
@@ -4,4 +4,5 @@ index = Blueprint("index", __name__, static_folder="static", template_folder="te
 
 @index.route("/")
 def home_index():
-    return render_template("index.html")
+    # return render_template("index.html")
+    return render_template("Authentication.html")
