9708e7ddc048e16e810197941bb7216fd3dd5b6e
AndrewBracero <a.bracero777@gmail.com>
2022-12-05 00:56:58 -0500

    added fixtures for recipe testing

--- tests/unit/test_recipes.py
+++ tests/unit/test_recipes.py
@@ -1,3 +1,31 @@
-def test_recipes(client):
-    response = client.get("/recipes")
-    assert b"<title>ShakeMate | Recipes</title>" in response.data # b next to it because it is a byte type; could be converted but easier to do this way
+import pytest
+from app.DatabaseComponent import User, recipe_table
+from app.__init__ import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    return app
+
+
+def test_new_recipe():
+    """
+    checks if a recipe is initialized correctly 
+    """
+    recipe = recipe_table(name='testRecipe', calories=350, fat=20, sugar=50)
+    assert recipe.name == 'testRecipe'
+    assert recipe.calories == 350
+    assert recipe.fat == 20
+    assert recipe.sugar == 50
+
+
+def test_recipe_with_fixture(new_recipe):
+    """
+    checks if a recipe is initialized correctly
+    uses fixture
+    """
+    assert new_recipe.name == 'testRecipe'
+    assert new_recipe.calories == 350
+    assert new_recipe.fat == 20
+    assert new_recipe.sugar == 50
